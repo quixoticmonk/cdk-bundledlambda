c6a865af8203f7be6c5ce891b0f33865065c72bc
Manu Chandrasekhar <manuchandrasekhar@gmail.com>
2021-05-04 23:31:39 -0400

    initial commit

--- cdk_bundledlambda/cdk_bundledlambda_stack.py
+++ cdk_bundledlambda/cdk_bundledlambda_stack.py
@@ -1,10 +1,4 @@
-from aws_cdk import core as cdk
-
-# For consistency with other languages, `cdk` is the preferred import name for
-# the CDK's core module.  The following line also imports it as `core` for use
-# with examples from the CDK Developer's Guide, which are in the process of
-# being updated to use `cdk`.  You may delete this import if you don't need it.
-from aws_cdk import core
+from aws_cdk import core as cdk, aws_lambda as _lambda
 
 
 class CdkBundledlambdaStack(cdk.Stack):
@@ -12,4 +6,19 @@ class CdkBundledlambdaStack(cdk.Stack):
     def __init__(self, scope: cdk.Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
+        _lambda.Function(
+            self,
+            "fn1",
+            handler="index.handler",
+            runtime=_lambda.Runtime.PYTHON_3_8,
+            timeout=cdk.Duration.seconds(10),
+            tracing=_lambda.Tracing.ACTIVE,
+            code=_lambda.Code.from_asset(
+                path="",
+                bundling=cdk.BundlingOptions(
+                    image=_lambda.Runtime.PYTHON_3_8.bundling_image,
+                    command=["bash", "c",
+                             " cd src && pip install -r requirements.txt -t /asset-output && cp -au . /asset-output"]
+                )
+            )
+        )
